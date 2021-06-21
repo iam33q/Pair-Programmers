d5a322be2ff50f2c1473afae5215bb32c09a3ba2
svtranter <82532670+svtranter@users.noreply.github.com>
2021-06-21 20:41:46 +0100

    Added more tests

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,9 +1,20 @@
 function deadOrAlive(cellStatus, neighbours) {
 
-    if (cellStatus === 1 && neighbours >= 2) {
-        return true
+    if (cellStatus === 1) {
+        if (neighbours < 2) {
+            return false
+        } else if (neighbours > 3) {
+            return false
+        } else {
+            return true
+        }
+        
     } else {
-        return false
+        if (neighbours === 3) {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
--- gameOfLife.spec.js
+++ gameOfLife.spec.js
@@ -1,8 +1,24 @@
 import { deadOrAlive } from "./gameOfLife"
 
 describe("cellStatus", () => {
-    it("has fewer than 2 live neighbours, it dies", () => {
+    it("is a live cell it has fewer than 2 live neighbours, it dies", () => {
         const result = deadOrAlive(1, 1);
+        expect(result).toBe(false);
+    }),
+    it("is a live cell it has more than 3 live neighbours, it dies", () => {
+        const result = deadOrAlive(1, 5);
+        expect(result).toBe(false);
+    }),
+    it("is a live cell it has 2 or 3 neighbours, it lives", () => {
+        const result = deadOrAlive(1, 2);
+        expect(result).toBe(true);
+    }),
+    it("is a dead cell with exactly 3 live neighbours, it lives", () => {
+        const result = deadOrAlive(0, 3);
+        expect(result).toBe(true);
+    }),
+    it("is a dead cell with not exactly 3 live neighbours, it stays dead", () => {
+        const result = deadOrAlive(0, 1);
         expect(result).toBe(false);
     })
 })
